84561e9c6a99fc391dce4b507e4abbb4a7718092
erika2C <128616925+erika2C@users.noreply.github.com>
2024-03-26 12:37:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteuso");
 
 //laço de repetição
 
@@ -6,7 +7,10 @@ for(let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            botaoes [j].classList.remove("ativo");
+            
         }
         botoes[i].classList.add("ativo");
     }
 }
+
