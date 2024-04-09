f4785f7ee2a652f67ae893cf2e6ea2621dcc4fc4
erika2C <128616925+erika2C@users.noreply.github.com>
2024-04-09 12:03:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,26 @@ const textos = document.querySelectorAll(".aba-conteuso");
 
 //laço de repetição
 
-for(let i = 0; i < botoes.length; i++){
-    botoes[i].onclick = function(){
-        for (let j = 0; j < botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
-            botaoes [j].classList.remove("ativo");
-            
+            textos[j].classList.remove("ativo");
+
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
 
+const contadores = document.querySelectorAll(".cantador");
+const tempoObjetivol = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivol - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempoObjetivol);
+function calculaTempo(tempoObjetivol) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivol - tempoAtual;
+}
